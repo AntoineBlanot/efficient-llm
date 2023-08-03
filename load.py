def1eab7a0f0bf8e09588772d9f9b185ab9e6bf3
Antoine BLANOT <antoineblanot@gmail.com>
2023-08-03 17:46:35 +0900

    update loading

--- load.py
+++ load.py
@@ -4,12 +4,12 @@ from accelerate import init_empty_weights, load_checkpoint_in_model
 
 
 def classic_load(path: str, device: str = "cpu"):
-    config = LlamaConfig.from_pretrained(checkpoint)
+    config = LlamaConfig.from_pretrained(path)
     model = LlamaForCausalLM.from_pretrained(path, torch_dtype=config.torch_dtype, device_map={"": device})
     return model
 
 def efficient_load(path: str, device: str = "cpu"):
-    config = LlamaConfig.from_pretrained(checkpoint)
+    config = LlamaConfig.from_pretrained(path)
     weights_location = snapshot_download(repo_id=path)
 
     with init_empty_weights():
@@ -21,8 +21,13 @@ def efficient_load(path: str, device: str = "cpu"):
 
 if __name__ == "__main__":
 
-    checkpoint = "Mikael110/llama-2-7b-guanaco-fp16"
-    model = efficient_load(path=checkpoint, device="cuda")
+    checkpoint = "Mikael110/llama-2-13b-guanaco-fp16"
+    device = "cpu"
+
+    model = efficient_load(path=checkpoint, device=device)
+    # model = classic_load(path=checkpoint, device=device)
+    
     print(model)
     print(model.model.layers[0].self_attn.q_proj.weight)
+    print(model.lm_head.weight)
     input()
