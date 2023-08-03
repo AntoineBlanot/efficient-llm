12d371076f3cd5dab70854b3aaf93971ba8e1ed0
Antoine BLANOT <antoineblanot@gmail.com>
2023-08-03 17:37:23 +0900

    update loading

--- load.py
+++ load.py
@@ -1,15 +1,28 @@
 from transformers import LlamaConfig, LlamaForCausalLM
 from huggingface_hub import snapshot_download
-from accelerate import init_empty_weights, load_checkpoint_and_dispatch
+from accelerate import init_empty_weights, load_checkpoint_in_model
 
-checkpoint = "Mikael110/llama-2-7b-guanaco-fp16"
-weights_location = snapshot_download(repo_id=checkpoint)
 
-config = LlamaConfig.from_pretrained(checkpoint)
-with init_empty_weights():
-    model = LlamaForCausalLM(config)
+def classic_load(path: str, device: str = "cpu"):
+    config = LlamaConfig.from_pretrained(checkpoint)
+    model = LlamaForCausalLM.from_pretrained(path, torch_dtype=config.torch_dtype, device_map={"": device})
+    return model
 
-print(model)
+def efficient_load(path: str, device: str = "cpu"):
+    config = LlamaConfig.from_pretrained(checkpoint)
+    weights_location = snapshot_download(repo_id=path)
 
-model = load_checkpoint_and_dispatch(model, checkpoint=weights_location)
-print(model)
+    with init_empty_weights():
+        model = LlamaForCausalLM(config)
+    
+    load_checkpoint_in_model(model, checkpoint=weights_location, dtype=config.torch_dtype, device_map={"": device})
+    return model
+
+
+if __name__ == "__main__":
+
+    checkpoint = "Mikael110/llama-2-7b-guanaco-fp16"
+    model = efficient_load(path=checkpoint, device="cuda")
+    print(model)
+    print(model.model.layers[0].self_attn.q_proj.weight)
+    input()
